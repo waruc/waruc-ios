fc09f007b921b6a1176f16a9be628b06a4360e80
ishansaksena <ishansaksena@gmail.com>
2017-04-07 18:52:54 -0700

    Horizontal pagination complete

--- ios-app/ios-app/ViewController.swift
+++ ios-app/ios-app/ViewController.swift
@@ -10,9 +10,45 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var scrollContainer: UIScrollView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // Adding the Profile View
+        let profileView: ProfileViewController = ProfileViewController(nibName: "ProfileViewController", bundle: nil)
+        
+        self.addChildViewController(profileView)
+        self.scrollContainer.addSubview(profileView.view)
+        profileView.didMove(toParentViewController: self)
+        
+        // Adding the Home View
+        let homeView: HomeViewController = HomeViewController(nibName: "HomeViewController", bundle: nil)
+        
+        self.addChildViewController(homeView)
+        self.scrollContainer.addSubview(homeView.view)
+        homeView.didMove(toParentViewController: self)
+
+        // Adding the Settings View
+        let settingsView: SettingsViewController = SettingsViewController(nibName: "SettingsViewController", bundle: nil)
+        
+        self.addChildViewController(settingsView)
+        self.scrollContainer.addSubview(settingsView.view)
+        settingsView.didMove(toParentViewController: self)
+        
+        // Adjusting frames
+        // Home
+        var homeFrame: CGRect = self.view.frame
+        homeFrame.origin.x = self.view.frame.width
+        homeView.view.frame = homeFrame
+        
+        // Settings
+        var settingsFrame: CGRect = self.view.frame
+        settingsFrame.origin.x = self.view.frame.width * 2
+        settingsView.view.frame = settingsFrame
+        
+        // Setting size of main scrollContainer
+        let viewCount: CGFloat = 3
+        self.scrollContainer.contentSize = CGSize(width: self.view.frame.width * viewCount, height: self.view.frame.height)
     }
 
     override func didReceiveMemoryWarning() {
