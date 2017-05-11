09e4c099e8364e5907d05b1baca0774e5d162141
JockFax32 <foxtjack@gmail.com>
2017-05-11 15:45:15 -0700

    create json(s)

---
+++ ios-app/ios-app/HomeViewController.swift
@@ -0,0 +1,20 @@
+//
+//  HomeViewController.swift
+//  ios-app
+//
+//  Created by Jack Fox on 5/6/17.
+//  Copyright © 2017 Nicholas Nordale. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+}
--- ios-app/ios-app/ViewController.swift
+++ ios-app/ios-app/ViewController.swift
@@ -8,37 +8,41 @@
 
 import UIKit
 import FirebaseAuth
-
+import FirebaseDatabase
+import SwiftyJSON
 
 
 class ViewController: UIViewController {
     
+    // Outlets
     @IBOutlet weak var signinSelector: UISegmentedControl!
-    
     @IBOutlet weak var signInLabel: UILabel!
-    
     @IBOutlet weak var emailTextField: UITextField!
-    
     @IBOutlet weak var passwordTextField: UITextField!
-    
     @IBOutlet weak var signInButton: UIButton!
+    @IBOutlet weak var logOutButton: UIButton!
+    @IBOutlet weak var backButton: UIButton!
     
+    var ref: FIRDatabaseReference!
+    var isSignIn:Bool = true
+    
+    // On app load
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        ref = FIRDatabase.database().reference()
+//        getUserVechiles()
+//        getUserProfile()
+//        trobuleShoot()
+        troubleV2()
     }
     
-    var isSignIn:Bool = true
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+
+    @IBAction func backButtonTapped(_ sender: UIButton) {
     }
-
     @IBAction func signInSelectorChanged(_ sender: UISegmentedControl) {
-        
+    
         isSignIn = !isSignIn
-        
         //Check the bool and set the button and labels
         if isSignIn {
             signInLabel.text = "Sign In"
@@ -65,7 +69,7 @@ class ViewController: UIViewController {
                         self.performSegue(withIdentifier: "goToSuccess", sender: self)
                     }
                     else {
-                        
+                        print("Troubles in Paradise")
                     }
                     
                 })
@@ -81,6 +85,7 @@ class ViewController: UIViewController {
                     }
                     else {
                         // display error
+                        print("You done fucked up")
                     }
                 })
             }
@@ -88,11 +93,113 @@ class ViewController: UIViewController {
         
     }
     
+    // Logout User
+    @IBAction func logOutUser(_ sender: Any) {
+        do {
+            try FIRAuth.auth()?.signOut()
+            self.performSegue(withIdentifier: "returnHome", sender: self)
+        } catch let logoutError {
+            print (logoutError)
+        }
+    }
+
+
+    // Remove on iPhone keyboard. Quality of life dev tool
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         
         // Dismiss the keyboard when the view is tapped on
         emailTextField.resignFirstResponder()
         passwordTextField.resignFirstResponder()
     }
+    
+    // retrieve user vehcile data on login
+    func getUserVechiles() -> Array<Any> {
+        if FIRAuth.auth()?.currentUser?.uid != nil {
+            // fetch data from Firebase
+            let uid = FIRAuth.auth()?.currentUser?.uid
+            ref.child("userVehicles").child(uid!).observeSingleEvent(of: .value, with: { (snapshot) in
+                var vhecile_keys = [String]()
+                let enumerator = snapshot.children
+                while let rest = enumerator.nextObject() as? FIRDataSnapshot{
+                    vhecile_keys.append(rest.key)
+                }
+                print(vhecile_keys)
+            }, withCancel: nil)
+        } else {
+            logOutUser(self)
+        }
+        return []
+    }
+    
+    func getUserProfile() {
+        if FIRAuth.auth()?.currentUser?.uid != nil {
+            let uid = FIRAuth.auth()?.currentUser?.uid
+            ref.child("vehicles/0/users").child(uid!).observeSingleEvent(of: .value, with: { (snapshot) in
+//                print(snapshot.value)
+                let dic = snapshot.value as? [String: AnyObject]
+                let values = dic!["devices"] as? NSArray
+                print(values!)
+//                print(dic!)
+//                print(values!.value)
+                
+            }, withCancel: nil)
+            
+        }
+    }
+    
+    func trobuleShoot() {
+        if FIRAuth.auth()?.currentUser?.uid != nil {
+            let uid = FIRAuth.auth()?.currentUser?.uid
+            ref.child("vehicles").observeSingleEvent(of: .value, with: { (snapshot) in
+               let enumerator = snapshot.children
+                while let rest = enumerator.nextObject() as? FIRDataSnapshot {
+                    if rest.hasChild("users/" + uid!) {
+                        let child_snap = rest.childSnapshot(forPath: "users/" + uid!)
+                    }
+                }
+                
+            }, withCancel: nil)
+            
+        }
+    }
+    
+    func troubleV2() {
+        let uid = FIRAuth.auth()?.currentUser?.uid //function argument
+        let dummy_keys: [String] = ["0", "1", "2"] // will call getUserVechiles()
+
+        for key in dummy_keys {
+            ref.child("vehicles/" + key + "/users/" + uid!).observeSingleEvent(of: .value, with: { (snapshot) in
+//                print(snapshot.ref)
+                let user_json = JSON(snapshot.value)
+                print(user_json)
+            }, withCancel: nil)
+            
+            ref.child("vehicles/" + key).observeSingleEvent(of: .value, with: { (snapshot) in
+                var vehicle_json = JSON(snapshot.value)
+                vehicle_json.dictionaryObject?.removeValue(forKey: "users")
+                print(vehicle_json)
+            }, withCancel: nil)
+        }
+    }
+    
+    
 }
+
+
+
+
+
+
+//                let vechile_dic = snapshot.value as? [String: AnyObject]
+//                print(vechile_dic?.values)
+
+// return value and priority
+//                let export = snapshot.valueInExportFormat()
+//                print(export)
+//                let json = JSON(snapshot.value) as? [String: AnyObject]
+//                print(json)
+//let enumerator = snapshot.children
+//while let rest = enumerator.nextObject() as? FIRDataSnapshot {}
+
+
 
