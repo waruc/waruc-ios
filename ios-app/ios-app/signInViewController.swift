b1f9579c35b25a35a9211351c00b5222fbf47bef
JockFax32 <foxtjack@gmail.com>
2017-05-11 19:23:30 -0700

    read json(s)

--- ios-app/ios-app/signInViewController.swift
+++ ios-app/ios-app/signInViewController.swift
@@ -9,15 +9,24 @@
 import UIKit
 import FirebaseDatabase
 import FirebaseAuth
+import SwiftyJSON
+
 
 class signInViewController: UIViewController {
 
+    // Outlets
     @IBOutlet weak var emailText: UITextField!
     @IBOutlet weak var passwordText: UITextField!
     @IBOutlet weak var logInButton: UIButton!
     
+    // Class Variables
+    var ref: FIRDatabaseReference!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        ref = FIRDatabase.database().reference()
+        getUserJSON()
 
     }
 
@@ -37,4 +46,45 @@ class signInViewController: UIViewController {
                 })
         }
     }
+    
+    func getUserVehicles() -> Array<Any> {
+        if FIRAuth.auth()?.currentUser?.uid != nil {
+            // fetch data from Firebase
+            let uid = FIRAuth.auth()?.currentUser?.uid
+            ref.child("userVehicles").child(uid!).observeSingleEvent(of: .value, with: { (snapshot) in
+                var vhecile_keys = [String]()
+                let enumerator = snapshot.children
+                while let rest = enumerator.nextObject() as? FIRDataSnapshot{
+                    vhecile_keys.append(rest.key)
+                }
+                print(vhecile_keys)
+            }, withCancel: nil)
+        } else {
+            print("Error fetching user vehicles")
+        }
+        return []
+    }
+    
+    
+    func getUserJSON() {
+        let uid = FIRAuth.auth()?.currentUser?.uid //function argument
+        let dummy_keys: [String] = ["0", "1", "2"] // will call getUserVechiles()
+        
+        for key in dummy_keys {
+            ref.child("vehicles/" + key + "/users/" + uid!).observeSingleEvent(of: .value, with: { (snapshot) in
+                let user_json = JSON(snapshot.value)
+                print(user_json)
+            }, withCancel: nil)
+            
+            ref.child("vehicles/" + key).observeSingleEvent(of: .value, with: { (snapshot) in
+                var vehicle_json = JSON(snapshot.value)
+                vehicle_json.dictionaryObject?.removeValue(forKey: "users")
+                print(vehicle_json)
+            }, withCancel: nil)
+        }
+    }
+    
+    
+    
+    
 }
