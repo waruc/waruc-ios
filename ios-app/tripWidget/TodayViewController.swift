40b87aaa6f946553a1f552f8eb1a35f551980b23
ishansaksena <ishansaksena@gmail.com>
2017-05-13 14:56:06 -0700

    Set up widget

---
+++ ios-app/tripWidget/TodayViewController.swift
@@ -0,0 +1,43 @@
+//
+//  TodayViewController.swift
+//  tripWidget
+//
+//  Created by ishansaksena on 5/13/17.
+//  Copyright © 2017 Nicholas Nordale. All rights reserved.
+//
+
+import UIKit
+import NotificationCenter
+
+class TodayViewController: UIViewController, NCWidgetProviding {
+        
+    @IBOutlet weak var trackingLabel: UILabel!
+    @IBOutlet weak var tripLabel: UILabel!
+    @IBOutlet weak var distanceLabel: UILabel!
+    @IBOutlet weak var timeLabel: UILabel!
+    
+    
+    @IBOutlet weak var tripButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print(Colors.green)
+        // Do any additional setup after loading the view from its nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func widgetPerformUpdate(completionHandler: (@escaping (NCUpdateResult) -> Void)) {
+        // Perform any setup necessary in order to update the view.
+        
+        // If an error is encountered, use NCUpdateResult.Failed
+        // If there's no update required, use NCUpdateResult.NoData
+        // If there's an update, use NCUpdateResult.NewData
+        
+        completionHandler(NCUpdateResult.newData)
+    }
+    
+}
