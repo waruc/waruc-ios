d44eb9414450cd19d1385de9852662aea2e6e660
JockFax32 <foxtjack@gmail.com>
2017-05-16 23:43:37 -0700

    vehicle onboarding

--- ios-app/ios-app/createVehicleViewController.swift
+++ ios-app/ios-app/createVehicleViewController.swift
@@ -9,6 +9,7 @@
 import UIKit
 import FirebaseCore
 import FirebaseDatabase
+import FirebaseAuth
 
 
 class createVehicleViewController: UIViewController {
@@ -28,16 +29,44 @@ class createVehicleViewController: UIViewController {
 
     }
 
-    func getVehicle() {
+    func registerVehicle(vin: String, make: String, model: String, year: String) {
+        let uid = FIRAuth.auth()?.currentUser?.uid
+        let key = self.ref.child("vehicles").childByAutoId().key
+        
+        let vehicle_values = [
+            "brand" : make,
+            "model": model,
+            "year": year,
+            "users": uid!,
+            "vin": vin,
+            "nickname": nicknameTextInput.text!,
+            "startDatetime": FIRServerValue.timestamp()
+            ] as [String : Any]
+        
+        let user_values = [
+            "trips": ["placeholder": "na"],
+            "total_mileage": 0,
+            
+            ] as [String : Any]
+        
+        ref.child("vehicles").updateChildValues([key : vehicle_values])
+        ref.child("vehicles/" + key + "/users").updateChildValues([uid! : user_values])
+        updateUserVehicles(key: key, uid: uid!)
         
     }
     
-    func registerVehicle() {
-    
+    func updateUserVehicles(key: String, uid: String) {
+        ref.child("userVehicles/" + uid + "/vehicles").updateChildValues([key: "owner"])
+        
+        // Remove an placeholder values that are created during the user onboarding process
+        ref.child("userVehicles/" + uid + "/vehicles").observeSingleEvent(of: .value, with: { (snapshot) in
+            if snapshot.hasChild("placeholder"){
+                self.ref.child("userVehicles/" + uid + "/vehicles/placeholder").removeValue()
+            }
+        }, withCancel: nil)
     }
     
-    
     @IBAction func submittTapped(_ sender: UIButton) {
-        registerVehicle()
+        registerVehicle(vin: "123456", make: "Ford", model: "F150", year: "1996")
     }
 }
