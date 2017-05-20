9a539707f94d93c9d7396d56f6fb31be976db939
dbabbs <dbabbs@uw.edu>
2017-05-20 14:52:51 -0700

    saving before ishan ruins everything

--- ios-app/ios-app/OnboardingAboutViewController.swift
+++ ios-app/ios-app/OnboardingAboutViewController.swift
@@ -10,8 +10,10 @@ import UIKit
 
 class OnboardingAboutViewController: UIViewController {
 
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        
 
         // Do any additional setup after loading the view.
     }
