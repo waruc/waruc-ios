8ffcef0b332dae37f2ebcc489ee4c94f9446a60b
dbabbs <dbabbs@uw.edu>
2017-05-24 17:05:36 -0700

    80% settings form color change

--- ios-app/ios-app/SettingsFormViewController.swift
+++ ios-app/ios-app/SettingsFormViewController.swift
@@ -23,9 +23,6 @@ class SettingsFormViewController: FormViewController, MFMailComposeViewControlle
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        //self.tableView?.backgroundColor = UIColor(red:0.14, green:0.13, blue:0.21, alpha:1.0)
-        //setBlack()
-        
         form +++ Section("Vehicles")
             
             <<< PickerInlineRow<String>("Current Car") { (row : PickerInlineRow<String>) -> Void in
@@ -35,6 +32,10 @@ class SettingsFormViewController: FormViewController, MFMailComposeViewControlle
                 }
                 row.options = ["BMW 725i", "Hummer H2", "Ferrari 458 Italia"]
                 row.value = row.options[0]
+                }.cellSetup() {cell, row in
+                    cell.backgroundColor = Colors.backgroundBlack
+                    cell.tintColor = UIColor.white
+                    cell.textLabel?.textColor = UIColor.white
             }
             
             <<< ButtonRow() { 
@@ -104,11 +105,11 @@ class SettingsFormViewController: FormViewController, MFMailComposeViewControlle
             
             +++ Section(header: "About", footer: "© 2017 Dylan Babbs, Jackson Brown, Jack Fox, Nick Nordale, and Ishan Saksena. All rights reserved.    ")
             
-
-            <<< ButtonRow("About the App") {
-                $0.title = $0.tag
-                $0.presentationMode = .segueName(segueName: "aboutApp", onDismiss: nil)
-            } 
+            //TODO: reinsert when website is running
+//            <<< ButtonRow("About the App") {
+//                $0.title = $0.tag
+//                $0.presentationMode = .segueName(segueName: "aboutApp", onDismiss: nil)
+//            } 
             <<< ButtonRow("About the Program") {
                 $0.title = $0.tag
                 $0.presentationMode = .segueName(segueName: "aboutProgram", onDismiss: nil)
@@ -136,8 +137,8 @@ class SettingsFormViewController: FormViewController, MFMailComposeViewControlle
                 .onCellSelection { cell, row in 
                     self.sendEmail()
             }
-        
     }
+
     
     override func viewWillAppear(_ animated: Bool) {
         self.navigationController?.setNavigationBarHidden(true, animated: animated)
@@ -157,11 +158,36 @@ class SettingsFormViewController: FormViewController, MFMailComposeViewControlle
     }
     
     func setBlack() {
+        print("set black occurred")
+        TextRow.defaultCellUpdate = { cell, row in 
+            cell.backgroundColor = Colors.backgroundBlack
+        }
+        ButtonRow.defaultCellUpdate = { cell, row in 
+            cell.backgroundColor = Colors.backgroundBlack
+        }
+        PasswordRow.defaultCellUpdate = { cell, row in 
+            cell.backgroundColor = Colors.backgroundBlack
+        }
+        PickerInlineRow<String>.defaultCellUpdate = { cell, row in 
+            cell.backgroundColor = Colors.backgroundBlack
+        }
         self.tableView?.backgroundColor = UIColor(red:0.14, green:0.13, blue:0.21, alpha:1.0)
     }
     
     func setWhite() {
         self.tableView?.backgroundColor = Colors.tableGrey
+        TextRow.defaultCellUpdate = { cell, row in 
+            cell.backgroundColor = Colors.white
+        }
+        ButtonRow.defaultCellUpdate = { cell, row in 
+            cell.backgroundColor = Colors.white
+        }
+        PasswordRow.defaultCellUpdate = { cell, row in 
+            cell.backgroundColor = Colors.white
+        }
+        PickerInlineRow<String>.defaultCellUpdate = { cell, row in 
+            cell.backgroundColor = Colors.white
+        }
     }
     
     func sendEmail() {      
@@ -193,4 +219,5 @@ class SettingsFormViewController: FormViewController, MFMailComposeViewControlle
         }
         UIApplication.shared.open(url, options: [:], completionHandler: completion)
     }
+    
 }
