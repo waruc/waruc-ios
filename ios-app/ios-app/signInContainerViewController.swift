6979fbbffc9dbd86c13dfc43d9bf0a26ad874220
NickNordale <nicknordale@gmail.com>
2017-05-24 18:35:33 -0700

    Password reset stuff

--- ios-app/ios-app/signInContainerViewController.swift
+++ ios-app/ios-app/signInContainerViewController.swift
@@ -13,11 +13,17 @@ import Eureka
 class signInContainerViewController: UIViewController {
 
     @IBOutlet weak var nextButton: UIButton!
+    @IBOutlet weak var forgotPasswordButton: UIButton!
+    
     var formViewController: signInFormViewController!
     
     let loginErrorAlert = UIAlertController(title: "Error", message: "That email & password combination is not valid!", preferredStyle: UIAlertControllerStyle.alert)
     let emailErrorAlert = UIAlertController(title: "Error", message: "Not a valid email", preferredStyle: UIAlertControllerStyle.alert)
     let passwordErrorAlert = UIAlertController(title: "Error", message: "Not a valid password", preferredStyle: UIAlertControllerStyle.alert)
+    
+    let passwordResetAlert = UIAlertController(title: "Reset Password", message: "Please enter your account email:", preferredStyle: .alert)
+    let passwordResetConfirmationAlert = UIAlertController(title: "Reset Password", message: "", preferredStyle: .alert)
+    let passwordResetErrorAlert = UIAlertController(title: "Error", message: "Error: email does not match a valid WARUC account.", preferredStyle: .alert)
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -28,9 +34,41 @@ class signInContainerViewController: UIViewController {
         let okAction = UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler: {(action: UIAlertAction!) in
             
         })
+        
+        let okResetAction = UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler: {(action: UIAlertAction!) in
+            self.performSegue(withIdentifier: "returnToVideo", sender: nil)
+        })
+        
         loginErrorAlert.addAction(okAction)
         emailErrorAlert.addAction(okAction)
         passwordErrorAlert.addAction(okAction)
+        passwordResetConfirmationAlert.addAction(okResetAction)
+        passwordResetErrorAlert.addAction(okAction)
+        
+        passwordResetAlert.addAction(UIAlertAction(title: "Reset", style: .default, handler: {
+            alert -> Void in
+            let email = self.passwordResetAlert.textFields![0] as UITextField
+            
+            FIRAuth.auth()?.sendPasswordReset(withEmail: email.text ?? "") { error in
+                if error == nil {
+                    self.passwordResetConfirmationAlert.message = "Password reset email sent to \(email.text!)"
+                    self.dismiss(animated: true, completion: nil)
+                    self.present(self.passwordResetConfirmationAlert, animated: true, completion: nil)
+                    
+                } else {
+                    self.dismiss(animated: true, completion: nil)
+                    self.present(self.passwordResetErrorAlert, animated: true, completion: {
+                        self.present(self.passwordResetAlert, animated: true, completion: nil)
+                    })
+                }
+            }
+        }))
+        
+        passwordResetAlert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        
+        passwordResetAlert.addTextField(configurationHandler: {(textField : UITextField!) -> Void in
+            textField.placeholder = "Email"
+        })
     }
     
     // Get a reference to the embedded view controller for the form
@@ -98,4 +136,9 @@ class signInContainerViewController: UIViewController {
     func startBLEScan() {
         BLERouter.sharedInstance.centralManager.scanForPeripherals(withServices: nil, options: nil)
     }
+    
+    @IBAction func resetPassword(_ sender: Any) {
+        present(passwordResetAlert, animated: true, completion: nil)
+    }
+    
 }
