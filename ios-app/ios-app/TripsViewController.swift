5d784cf0e65186fd19501caeb914b453a076cd37
NickNordale <nicknordale@gmail.com>
2017-05-26 20:05:04 -0700

    Fixed trips table distance label color change bug

--- ios-app/ios-app/TripsViewController.swift
+++ ios-app/ios-app/TripsViewController.swift
@@ -181,6 +181,8 @@ class TripsViewController: UIViewController, UITableViewDelegate, UITableViewDat
         cell.distanceLabel?.text = "\((((currTrip["distance"] as! Double) * 10).rounded() / 10)) miles"
         cell.monthLabel?.text = "\(calendar.shortMonthSymbols[calendar.component(.month, from: date) - 1])"
         
+        cell.distanceLabel.textColor = BLERouter.sharedInstance.tracking ? #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1) : #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+        
         return cell
     }
     
